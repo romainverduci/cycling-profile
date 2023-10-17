dcc02344b767ade9a4809316c4ef9bedefd58040
Romain Verduci <rverduci@cloudbees.com>
2023-10-17 23:44:47 +0200

    improve user profile

--- src/UserName.tsx
+++ src/UserName.tsx
@@ -1,9 +1,12 @@
+import Typography from "@mui/material/Typography";
 import { Athlete } from "./types";
+import { SxProps, Theme } from "@mui/material/styles";
 
 interface UserNameProps {
   athlete: Athlete;
+  sx?: SxProps<Theme>
 }
 
-export const UserName = ({ athlete }: UserNameProps) => {
-  return <div>{athlete.username}</div>;
+export const UserName = ({ athlete, ...otherProps }: UserNameProps) => {
+  return <Typography variant="h3" {...otherProps}>{`${athlete.firstname} ${athlete.lastname}`}</Typography>;
 };
--- src/UserPage.tsx
+++ src/UserPage.tsx
@@ -1,11 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import Avatar from "@mui/material/Avatar";
 import { UserName } from "./UserName";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 
 export const UserPage = ({ athlete }: { athlete: any }) => {
   return (
-    <>
-      <img src={athlete?.profile_medium} />
-      <UserName athlete={athlete!} />
-    </>
+    <Container sx={{ minHeight: "100vh" }}>
+      <Box display="flex" flexDirection='column'>
+        <Avatar
+          sx={{ width: 125, height: 125, alignSelf: 'center' }}
+          alt={athlete?.username}
+          src={athlete?.profile}
+        />
+        <UserName sx={{pt: 3}} athlete={athlete!} />
+      </Box>
+    </Container>
   );
 };
